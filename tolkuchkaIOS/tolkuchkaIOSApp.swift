85f79e0f8af3203cc03a97b9daf38f9b8d322253
durdysagty <durdysagty@mail.ru>
2023-05-06 01:41:46 +0500

    Initial Commit

---
+++ tolkuchkaIOS/tolkuchkaIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  tolkuchkaIOSApp.swift
+//  tolkuchkaIOS
+//
+//  Created by MacBook on 06.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct tolkuchkaIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
